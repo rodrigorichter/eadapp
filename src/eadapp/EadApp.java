c6c982e40e5852b788b7eb4d41cbe180d0a355a7
rrrichter <guigo_richter@hotmail.com>
2016-10-28 00:58:32 -0200

    .

--- src/eadapp/EadApp.java
+++ src/eadapp/EadApp.java
@@ -1,7 +1,22 @@
 package eadapp;
 
+import eadapp.data.Database;
+import eadapp.ui.UserInterface;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import eadapp.teste;
+
 public class EadApp {
+	public static UserInterface ui;
+	public static Database db;
+	
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		db = new Database(true);
+		ui = new UserInterface();
+		
+		
+		
 	}
 }
--- src/eadapp/ui/UserInterface.java
+++ src/eadapp/ui/UserInterface.java
@@ -1,5 +1,58 @@
 package eadapp.ui;
 
-public class UserInterface {
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
+public class UserInterface extends Composite {
+	public UserInterface(Composite parent, int style) {
+		super(parent,style);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void Login() {
+		
+		Button btnLogin = new Button(this, SWT.NONE);
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				System.out.println("MUITO");
+			}
+		});
+		btnLogin.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnLogin.setBounds(155, 261, 101, 29);
+		btnLogin.setText("Login");
+		
+		Display display = Display.getDefault();
+		Shell shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("EU ODEIO JAVA");
+		
+		
+		shell.open();
+		shell.layout();
+		
+		
+		while(!shell.isDisposed()) {
+			//ui.showUI();
+			//ui.getCommand();
+			
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+			
+		}
+		
+	}
+	
 }
